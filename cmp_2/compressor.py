2ac07786c1db3b230d5c6a0cd31ac6ea27c81bc4
RickSanj <116678653+RickSanj@users.noreply.github.com>
2023-05-26 16:36:31 +0300

    adjusted for txt file

--- cmp_2/compressor.py
+++ cmp_2/compressor.py
@@ -1,25 +1,72 @@
-from image_data import work_with_image
-# from text_to_string import work_with_text
-from video_data import work_with_video
-from audio_data import work_with_audio
+"""
+Module that handles
+the where to distribute the file
+compressing and decompressing of the file
+using to the required algorithm
+"""
+
+from Algorithms.lzw import LZW
 
 
-def compress(path, algorithm):
-    #function that returns path to the compressed file
-    return path
-
+def compress(path: str, algorithm: str):
+    """
+    Args:
+        - path: str, the path/name of file with extention
+        - algorithm: str, name of the required algorithm
+    Return:
+        - file_path: str, decompressed file path
+    """
     file_type = path.split('.')[-1]
-    if file_type == 'txt':
-        ...
+
+    # TEXT
+    if extention in ['txt']:
+        string = ''
+        #Read data from a file
+        with open(path, encoding="UTF-8") as file:
+            data = file.read()
+            data = '/'.join(data.split('\n'))
+        decompressed = find_algo(algorithm, data)
+        assert decompressed == data
+        with open('decompressed.txt', 'w', encoding='UTF-8') as file:
+            decompressed = decompressed.split('/')
+            for element in decompressed:
+                string += element + '\n'
+            file.write(string)
+            return 'decompressed.txt'
+
+    # PICTURE
     elif file_type in ['jpg', 'png', 'jpeg']:
         work_with_image(path, algorithm)
+    # VIDEO
     elif file_type == 'mp4':
         work_with_video(path, algorithm)
+    # AUDIO
     elif file_type in ['mp3', 'wav', 'flac']:
         work_with_audio(path, algorithm)
     else:
         print('File type not supported')
         return None
-
-
     return path
+
+
+def find_algo(name: str, data: str) -> str:
+    """find and use the required algo"""
+    if name == 'Huffman':
+        pass
+    if name == 'LZW':
+        lzw = LZW(data)
+        compressed = lzw.encode()
+        decompressed = LZW(compressed).decode()
+        assert data == decompressed
+        return decompressed
+    if name == 'LZ77':
+        pass
+    if name == 'Deflate':
+        pass
+    if name == 'Fifth algorithm':
+        pass
+
+text = compress("/Users/alina.babenko/Desktop/Дискретна мат/2_sem/computer_project_2/Alina/bible.txt", 'LZW')
+print(text)
+# current_directory = Path.cwd()
+# print(current_directory)
