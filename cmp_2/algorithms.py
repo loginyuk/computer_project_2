2db8d84ff86940fd3ceb7bb4089594fc27d23f7c
vbronetskyi <116158735+vbronetskyi@users.noreply.github.com>
2023-05-29 20:31:26 +0300

    Update algorithms.py

--- cmp_2/algorithms.py
+++ cmp_2/algorithms.py
@@ -2,7 +2,7 @@
 module that handles the compression of a str
 using the required algo, determines the effectiveness and time
 """
-
+import time
 from Algorithms.lzw import LZW
 from Algorithms.lz77 import LZ77
 from Algorithms.huffman import huffman_statistic
@@ -14,18 +14,32 @@ def lzw_handler(data):
     """
     LZW algorithm handler
     """
-    #compression ratio, compress time, decompress time
-    statistics, decompressed_data = get_compression_statistics(LZW(), data)
+    lzw = LZW()
+    compressed_text = lzw.encode(data)
+    decompressed_data = lzw.decode(compressed_text)
     assert decompressed_data == data
-    return decompressed_data, statistics
+    return decompressed_data, (lzw.compression_ratio, lzw.compression_time, lzw.decompression_time)
 
 def lz77_handler(data):
     """
     LZ77 algorithm handler
     """
-    statistics, decompressed_data = get_compression_statistics(LZ77(), data)
+    lz77 = LZ77()
+
+    # Стиснення
+    start_time = time.time()
+    compressed_data = lz77.encode(data)
+    compression_time = time.time() - start_time
+
+    # Розтиснення
+    start_time = time.time()
+    decompressed_data = lz77.decode(compressed_data)
+    decompression_time = time.time() - start_time
+
+    # Відсоток стиснення
+    compression_ratio = (1 - (len(compressed_data) / len(data))) * 100
     assert decompressed_data == data
-    return decompressed_data, statistics
+    return decompressed_data, (compression_ratio, compression_time, decompression_time)
 
 def huffman_handler(data):
     """
